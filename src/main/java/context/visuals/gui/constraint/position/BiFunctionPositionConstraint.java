1758272dd175b5d003c42116c4c661a6aa8d2d21
JaryJay <jay.jrjren@gmail.com>
2021-07-28 18:45:51 -0400

    Create BiFunctionPositionConstraint.java

---
+++ src/main/java/context/visuals/gui/constraint/position/BiFunctionPositionConstraint.java
@@ -0,0 +1,18 @@
+package context.visuals.gui.constraint.position;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionPositionConstraint extends GuiPositionConstraint {
+
+	private BiFunction<Float, Float, Float> biFunction;
+
+	public BiFunctionPositionConstraint(BiFunction<Float, Float, Float> biFunction) {
+		this.biFunction = biFunction;
+	}
+
+	@Override
+	public float calculateValue(float start, float end) {
+		return biFunction.apply(start, end);
+	}
+
+}
