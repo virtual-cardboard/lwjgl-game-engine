91ac0e9690d67bb37470f87aef9e9cbc6f471bd6
JaryJay <jay.jrjren@gmail.com>
2021-07-30 10:35:41 -0400

    Finished EllipseRenderer and its shaders

--- src/main/java/context/visuals/renderer/EllipseRenderer.java
+++ src/main/java/context/visuals/renderer/EllipseRenderer.java
@@ -25,25 +25,26 @@ public class EllipseRenderer extends GameRenderer {
 	}
 
 	public void render(RootGui rootGui, Vector2f center, Vector2f dimensions, int innerColour, float outerWidth) {
-		render(rootGui, center, dimensions, innerColour, 0, outerWidth);
+		render(rootGui, center, dimensions, innerColour, 255, outerWidth);
 	}
 
 	public void render(RootGui rootGui, Vector2f center, Vector2f dimensions, int innerColour, int outerColour, float outerWidth) {
 		shaderProgram.bind();
+		Vector2f rootGuiDimensions = rootGui.getDimensions();
 		Matrix4f transform = new Matrix4f();
 		transform.translate(NEG_ONE_ONE);
-		transform.scale(TWO_NEG_TWO.copy().divide(rootGui.getDimensions()));
+		transform.scale(TWO_NEG_TWO.copy().divide(rootGuiDimensions));
 		transform.translate(center.copy().sub(dimensions.copy().scale(0.5f)));
 		transform.scale(dimensions);
-
-		Vector2f transformedCenter = Matrix4f.transform(transform, new Vector2f(0.5f, 0.5f));
-		Vector2f transformedDimensions = new Vector2f(1, 1).scale(TWO_NEG_TWO.copy().divide(rootGui.getDimensions()));
+		shaderProgram.setMat4("transform", transform);
+		Vector2f transformedCenter = Matrix4f.transform(transform, new Vector2f(0.5f, 0.5f)); // [563.2188, -105.778656]
 		shaderProgram.setVec2("centerPos", transformedCenter);
+		Vector2f transformedDimensions = dimensions.copy().divide(rootGuiDimensions).scale(2);
 		shaderProgram.setVec2("dimensions", transformedDimensions);
-		System.out.println(transformedCenter + "     " + transformedDimensions);
 		shaderProgram.setVec4("innerColour", Colour.toNormalizedVector(innerColour));
 		shaderProgram.setVec4("outerColour", Colour.toNormalizedVector(outerColour));
-		shaderProgram.setFloat("outerWidth", outerWidth);
+		shaderProgram.setFloat("outerWidthX", 2 * outerWidth / rootGuiDimensions.x);
+		shaderProgram.setFloat("outerWidthY", 2 * outerWidth / rootGuiDimensions.y);
 		vao.display();
 	}
 
--- src/main/resources/shaders/ellipseFragmentShader.glsl
+++ src/main/resources/shaders/ellipseFragmentShader.glsl
@@ -6,20 +6,21 @@ uniform vec2 centerPos;
 uniform vec2 dimensions;
 uniform vec4 innerColour;
 uniform vec4 outerColour;
-uniform float outerWidth;
+uniform float outerWidthX;
+uniform float outerWidthY;
 
 void main() {
 	float xDiff = fragmentPos.x - centerPos.x;
 	float yDiff = fragmentPos.y - centerPos.y;
 	float halfWidth = dimensions.x * 0.5f;
 	float halfHeight = dimensions.y * 0.5f;
-    if (xDiff * xDiff / ((halfWidth - outerWidth) * (halfWidth - outerWidth)) +
-    	yDiff * yDiff / ((halfHeight - outerWidth) * (halfHeight - outerWidth)) <= 1) {
+    if (xDiff * xDiff / ((halfWidth - outerWidthX) * (halfWidth - outerWidthX)) +
+    	yDiff * yDiff / ((halfHeight - outerWidthY) * (halfHeight - outerWidthY)) <= 1) {
     	fragmentColor = innerColour;
     } else if (xDiff * xDiff / (halfWidth * halfWidth) +
     	yDiff * yDiff / (halfHeight * halfHeight) <= 1) {
     	fragmentColor = outerColour;
     } else {
-	    fragmentColor = vec4(0.1f, 0.5f, 0.7f, 0.1f);
+	    fragmentColor = vec4(0, 0, 0, 0);
     }
 }
