8439a9bc80fb9a9ad8eaadd454bba7904fd42a96
JaryJay <jay.jrjren@gmail.com>
2021-08-04 09:55:55 -0400

    Update GameRenderer.java

--- src/main/java/context/visuals/renderer/GameRenderer.java
+++ src/main/java/context/visuals/renderer/GameRenderer.java
@@ -3,7 +3,7 @@ package context.visuals.renderer;
 import context.visuals.GameVisuals;
 
 /**
- * Renders the game. Crate implementation(s) of this, and use them in
+ * Renders objects. Crate implementation(s) of this, and use them in
  * {@link GameVisuals}'s {@link GameVisuals#render() render()} function.
  * 
  * @author Jay
