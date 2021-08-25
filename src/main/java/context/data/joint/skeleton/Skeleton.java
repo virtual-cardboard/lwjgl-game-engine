4c2b8cdf0bcbdfd3f923321d59fac33c071bc6fc
JaryJay <jay.jrjren@gmail.com>
2021-08-25 10:40:06 -0400

    Created Skeleton and SkeletonNode

---
+++ src/main/java/context/data/joint/skeleton/Skeleton.java
@@ -0,0 +1,15 @@
+package context.data.joint.skeleton;
+
+public class Skeleton {
+
+	private SkeletonNode rootNode;
+
+	public Skeleton(SkeletonNode rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	public SkeletonNode getRootNode() {
+		return rootNode;
+	}
+
+}
