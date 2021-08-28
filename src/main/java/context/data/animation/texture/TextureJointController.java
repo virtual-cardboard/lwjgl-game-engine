ad4ea789db954e6b193884f4e5e2bad8aa5a28fb
JaryJay <jay.jrjren@gmail.com>
2021-08-28 13:39:26 -0400

    Update TextureJointController.java

--- src/main/java/context/data/animation/texture/TextureJointController.java
+++ src/main/java/context/data/animation/texture/TextureJointController.java
@@ -1,6 +1,6 @@
 package context.data.animation.texture;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import context.data.animation.joint.Joint;
@@ -8,16 +8,19 @@ import context.data.animation.joint.JointController;
 
 public class TextureJointController extends JointController {
 
-	public TextureJointController() {
-		this(new ArrayList<>());
+	private List<JointTexture> jointTextures;
+
+	public TextureJointController(List<JointTexture> jointTextures, Joint... joints) {
+		this(jointTextures, Arrays.asList(joints));
 	}
 
-	public TextureJointController(List<Joint> joints) {
+	public TextureJointController(List<JointTexture> jointTextures, List<Joint> joints) {
 		super(joints);
+		this.jointTextures = jointTextures;
 	}
 
 	@Override
-	public void doUpdateJoints(List<Joint> joints) {
+	public void updateJoints() {
 	}
 
 }
