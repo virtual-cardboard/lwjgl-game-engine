75b749a5852636d41f9809578d41b5ac0dfe3423
JaryJay <jay.jrjren@gmail.com>
2021-08-29 16:50:12 -0400

    Add totalNumNodes()

--- src/main/java/context/data/animation/skeleton/Skeleton.java
+++ src/main/java/context/data/animation/skeleton/Skeleton.java
@@ -1,5 +1,7 @@
 package context.data.animation.skeleton;
 
+import java.util.List;
+
 public class Skeleton {
 
 	private SkeletonNode rootNode;
@@ -14,6 +16,20 @@ public class Skeleton {
 
 	public SkeletonNode getRootNode() {
 		return rootNode;
+	}
+
+	public int totalNumNodes() {
+		return doTotalNumNodes(rootNode);
+	}
+
+	private int doTotalNumNodes(SkeletonNode node) {
+		int num = 1;
+		List<SkeletonNode> children = node.getChildren();
+		for (int i = 0; i < children.size(); i++) {
+			SkeletonNode child = children.get(i);
+			num += doTotalNumNodes(child);
+		}
+		return num;
 	}
 
 }
