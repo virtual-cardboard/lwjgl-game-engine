83f49ab5cb2f6497a5591e684523b12bf7ce3bcc
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-10-08 12:34:09 -0400

    Update GameLoaderLinker to call glFinish()
    Also added some javadocs to GameLoaderLinker

--- src/main/java/common/loader/GameLoaderLinker.java
+++ src/main/java/common/loader/GameLoaderLinker.java
@@ -2,13 +2,22 @@ package common.loader;
 
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.opengl.GL.createCapabilities;
+import static org.lwjgl.opengl.GL11.glFinish;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
 
 import common.loader.loadtask.OpenglLoadTask;
 
-public class GameLoaderLinker implements Runnable {
+/**
+ * A loader thread used for OpenGL function calls. Uses a shared context with
+ * the rendering thread so that all non-container objects can be directly used
+ * in the rendering thread.
+ * 
+ * @author Jay
+ *
+ */
+public final class GameLoaderLinker implements Runnable {
 
 	private boolean running = true;
 	private Queue<OpenglLoadTask> openglLoadTasks = new ArrayDeque<>();
@@ -26,6 +35,9 @@ public class GameLoaderLinker implements Runnable {
 		while (running) {
 			if (lt != null) {
 				lt.loadOpengl();
+				// Must call glFinish, or else the loader thread buffer this command until the
+				// next glSwapBuffers() call - which would never happen.
+				glFinish();
 				lt.countDownLatch().countDown();
 			} else {
 				Thread.yield();
@@ -34,10 +46,20 @@ public class GameLoaderLinker implements Runnable {
 		}
 	}
 
+	/**
+	 * Adds an {@link OpenglLoadTask} to a queue. The load task would be executed
+	 * some time in the future. Added load tasks would be executed in the order that
+	 * they are added.
+	 * 
+	 * @param t the <code>OpenglLoadTask</code> to add
+	 */
 	public void add(OpenglLoadTask t) {
 		openglLoadTasks.add(t);
 	}
 
+	/**
+	 * Stops the loader linker.
+	 */
 	public void terminate() {
 		running = false;
 	}
