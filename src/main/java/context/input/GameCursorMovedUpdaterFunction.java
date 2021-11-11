1480a4e855a98639f752d6598bb79a91e28b7279
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-11-11 12:09:44 -0500

    Refactor GameCursor

--- src/main/java/context/input/GameCursorMovedUpdaterFunction.java
+++ src/main/java/context/input/GameCursorMovedUpdaterFunction.java
@@ -16,7 +16,7 @@ class GameCursorMovedUpdaterFunction implements Function<MouseMovedInputEvent, G
 
 	@Override
 	public GameEvent apply(MouseMovedInputEvent inputEvent) {
-		cursor.setCursorCoordinates(inputEvent.x(), inputEvent.y());
+		cursor.setPos(inputEvent.x(), inputEvent.y());
 		return null;
 	}
 
--- src/main/java/context/input/mouse/GameCursor.java
+++ src/main/java/context/input/mouse/GameCursor.java
@@ -4,24 +4,29 @@ import common.math.Vector2f;
 
 public class GameCursor {
 
-	private Vector2f cursorCoordinates;
-	private boolean[] pressedButtons;
+	private Vector2f previousPos = new Vector2f();
+	private Vector2f pos = new Vector2f();
+	private boolean[] pressedButtons = new boolean[8];
 
-	public GameCursor() {
-		cursorCoordinates = new Vector2f();
-		pressedButtons = new boolean[8];
+	public Vector2f previousPos() {
+		return previousPos;
 	}
 
-	public Vector2f coordinates() {
-		return cursorCoordinates;
+	public Vector2f pos() {
+		return pos;
 	}
 
-	public void setCursorCoordinates(Vector2f cursorCoordinates) {
-		this.cursorCoordinates.set(cursorCoordinates);
+	public Vector2f velocity() {
+		return pos.copy().sub(previousPos);
 	}
 
-	public void setCursorCoordinates(int x, int y) {
-		this.cursorCoordinates.set(x, y);
+	public void setPos(Vector2f pos) {
+		setPos(pos.x, pos.y);
+	}
+
+	public void setPos(float x, float y) {
+		previousPos.set(pos);
+		this.pos.set(x, y);
 	}
 
 	public boolean leftButtonPressed() {
