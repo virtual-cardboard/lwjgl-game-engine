55d8acadb14ab1ec0a8156eda6d0ab8b970aae1d
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-12-01 19:53:08 -0500

    Update GameRenderer javadocs
    A GameRenderer uses ShaderPrograms, VertexArrayObjects, andother GameRenderers to render objects onto the screen. Use GameRenderers in GameVisuals's render() function.

--- src/main/java/context/visuals/renderer/GameRenderer.java
+++ src/main/java/context/visuals/renderer/GameRenderer.java
@@ -1,14 +1,17 @@
 package context.visuals.renderer;
 
 import context.visuals.GameVisuals;
+import context.visuals.lwjgl.ShaderProgram;
+import context.visuals.lwjgl.VertexArrayObject;
 
 /**
- * Renders objects. Crate implementation(s) of this, and use them in
- * {@link GameVisuals}'s {@link GameVisuals#render() render()} function.
+ * A GameRenderer uses {@link ShaderProgram}s, {@link VertexArrayObject}s, and
+ * other <code>GameRenderer</code>s to render objects onto the screen. Use
+ * <code>GameRenderer</code>s in {@link GameVisuals}'s
+ * {@link GameVisuals#render() render()} function.
  * 
  * @author Jay
  *
  */
 public abstract class GameRenderer {
-
 }
