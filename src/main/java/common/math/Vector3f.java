19c13e7b52bb2473383673cf559382695ce80143
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-12-10 12:29:50 -0500

    Create X_AXIS and Y_AXIS constants

--- src/main/java/common/math/Vector3f.java
+++ src/main/java/common/math/Vector3f.java
@@ -9,6 +9,8 @@ import java.util.Objects;
  */
 public class Vector3f {
 
+	public static final Vector3f X_AXIS = new Vector3f(1, 0, 0);
+	public static final Vector3f Y_AXIS = new Vector3f(0, 1, 0);
 	public static final Vector3f Z_AXIS = new Vector3f(0, 0, 1);
 
 	public final float x;
