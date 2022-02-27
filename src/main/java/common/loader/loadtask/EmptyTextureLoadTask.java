2dc5e6135452e458201b0e602a07728c23bb067c
Jay <jay.jrjren@gmail.com>
2022-02-27 15:04:39 -0500

    EmptyTextureLoadTask clamp edges

--- src/main/java/common/loader/loadtask/EmptyTextureLoadTask.java
+++ src/main/java/common/loader/loadtask/EmptyTextureLoadTask.java
@@ -1,6 +1,7 @@
 package common.loader.loadtask;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -25,6 +26,8 @@ public class EmptyTextureLoadTask extends GLLoadTask<Texture> {
 		texture.bind(glContext);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		texture.setWidth(width);
