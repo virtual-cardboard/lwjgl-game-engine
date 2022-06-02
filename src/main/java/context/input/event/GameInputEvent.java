9306775c9872cd62644c9dda47a9265b74e09b50
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-06-02 09:36:31 -0400

    Delete GameEvent time field, add to GameInputEvent

--- src/main/java/context/input/event/GameInputEvent.java
+++ src/main/java/context/input/event/GameInputEvent.java
@@ -1,14 +1,20 @@
 package context.input.event;
 
+import static java.lang.System.currentTimeMillis;
+
 import engine.common.event.GameEvent;
 
-public abstract class GameInputEvent extends GameEvent {
+public abstract class GameInputEvent extends GameEvent implements Comparable<GameInputEvent> {
+
+	private final long time;
 
 	public GameInputEvent() {
+		this.time = currentTimeMillis();
 	}
 
-	public GameInputEvent(long time) {
-		super(time);
+	@Override
+	public int compareTo(GameInputEvent o) {
+		return Long.compare(time, o.time);
 	}
 
 }
--- src/main/java/engine/common/event/GameEvent.java
+++ src/main/java/engine/common/event/GameEvent.java
@@ -1,30 +1,8 @@
 package engine.common.event;
 
-import static java.lang.System.currentTimeMillis;
-
-public abstract class GameEvent implements Comparable<GameEvent> {
-
-	protected long time;
-
-	public GameEvent(long time) {
-		this.time = time;
-	}
-
-	public GameEvent() {
-		this(currentTimeMillis());
-	}
-
-	protected void setTime(long time) {
-		this.time = time;
-	}
-
-	public long time() {
-		return time;
-	}
-
-	@Override
-	public int compareTo(GameEvent other) {
-		return Long.compare(this.time, other.time);
-	}
+/**
+ * An event in the game.
+ */
+public abstract class GameEvent {
 
 }
