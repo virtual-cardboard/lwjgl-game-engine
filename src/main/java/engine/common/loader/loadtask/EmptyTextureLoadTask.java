1a4b65ea65fef95e1eeb091bbdf484ceab89020a
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-07-08 09:36:52 -0400

    Add TextureLoadTask, parameters
    Fix bug where text would fade out when rotated/scaled/transformed by a matrix

--- src/main/java/engine/common/loader/loadtask/EmptyTextureLoadTask.java
+++ src/main/java/engine/common/loader/loadtask/EmptyTextureLoadTask.java
@@ -1,7 +1,6 @@
 package engine.common.loader.loadtask;
 
 import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_LINEAR_MIPMAP_LINEAR;
 import static org.lwjgl.opengl.GL11.GL_RGBA;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
@@ -15,6 +14,9 @@ import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import context.GLContext;
 import context.visuals.lwjgl.Texture;
 import engine.common.loader.GLLoadTask;
@@ -24,7 +26,13 @@ public class EmptyTextureLoadTask implements GLLoadTask<Texture> {
 	private int width;
 	private int height;
 
+	private Map<Integer, Integer> textureParameters = new HashMap<>();
+
 	public EmptyTextureLoadTask(int width, int height) {
+		setTextureParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		setTextureParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		setTextureParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		setTextureParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		this.width = width;
 		this.height = height;
 	}
@@ -36,13 +44,22 @@ public class EmptyTextureLoadTask implements GLLoadTask<Texture> {
 		texture.bind(glContext);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 		glGenerateMipmap(GL_TEXTURE_2D);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		for (Integer key : textureParameters.keySet()) {
+			glTexParameteri(GL_TEXTURE_2D, key, textureParameters.get(key));
+		}
 		texture.setWidth(width);
 		texture.setHeight(height);
 		return texture;
+	}
+
+	/**
+	 * Set a GL texture parameter.
+	 *
+	 * @param key the parameter's key, e.g. GL_TEXTURE_MIN_FILTER
+	 * @param val the parameter's value, e.g. GL_LINEAR
+	 */
+	public void setTextureParameter(int key, int val) {
+		textureParameters.put(key, val);
 	}
 
 }
--- src/main/java/engine/common/loader/loadtask/TextureLoadTask.java
+++ src/main/java/engine/common/loader/loadtask/TextureLoadTask.java
@@ -1,6 +1,15 @@
 package engine.common.loader.loadtask;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
+import static org.lwjgl.opengl.GL11.GL_RGBA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_S;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_T;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
 import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 import static org.lwjgl.stb.STBImage.stbi_failure_reason;
@@ -9,22 +18,25 @@ import static org.lwjgl.stb.STBImage.stbi_load;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-
-import org.lwjgl.system.MemoryStack;
+import java.util.HashMap;
+import java.util.Map;
 
 import context.GLContext;
 import context.visuals.lwjgl.Texture;
 import engine.common.loader.GLLoadTask;
 import engine.common.loader.graph.loader.GLLoader0Arg;
+import org.lwjgl.system.MemoryStack;
 
 public class TextureLoadTask implements GLLoadTask<Texture>, GLLoader0Arg<Texture> {
 
 	private Texture texture;
 	private String path;
 
+	private Map<Integer, Integer> textureParameters = new HashMap<>();
+
 	/**
 	 * Creates a <code>TextureLoadTask</code> with a texture unit and a path.
-	 * 
+	 *
 	 * @param textureUnit the texture to load data into
 	 * @param path        the path to the texture's source image
 	 */
@@ -34,11 +46,15 @@ public class TextureLoadTask implements GLLoadTask<Texture>, GLLoader0Arg<Textur
 
 	/**
 	 * Creates a <code>TextureLoadTask</code> with an unloaded texture and a path.
-	 * 
+	 *
 	 * @param texture the texture to load data into
 	 * @param path    the path to the texture's source image
 	 */
 	public TextureLoadTask(Texture texture, String path) {
+		setTextureParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		setTextureParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		setTextureParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		setTextureParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		this.texture = texture;
 		this.path = path;
 	}
@@ -62,12 +78,21 @@ public class TextureLoadTask implements GLLoadTask<Texture>, GLLoader0Arg<Textur
 		texture.bind(glContext);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width(), texture.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
 		glGenerateMipmap(GL_TEXTURE_2D);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		for (Integer key : textureParameters.keySet()) {
+			glTexParameteri(GL_TEXTURE_2D, key, textureParameters.get(key));
+		}
 		stbi_image_free(textureData);
 		return texture;
+	}
+
+	/**
+	 * Set a GL texture parameter.
+	 *
+	 * @param key the parameter's key, e.g. GL_TEXTURE_MIN_FILTER
+	 * @param val the parameter's value, e.g. GL_LINEAR
+	 */
+	public void setTextureParameter(int key, int val) {
+		textureParameters.put(key, val);
 	}
 
 }
