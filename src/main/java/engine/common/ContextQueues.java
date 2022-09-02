d7057b8a0c4d5ed3bd4773705601ad04ea578c42
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-09-02 11:24:15 -0400

    Use ConcurrentLinkedQueue instead of ArrayBlockingQueue
    in ContextQueues. This is because ArrayBlockingQueue throws an exception if you try to add an element when it's full. On the other hand, ConcurrentLinkedQueues have no size limit.

--- src/main/java/engine/common/ContextQueues.java
+++ src/main/java/engine/common/ContextQueues.java
@@ -3,7 +3,7 @@ package engine.common;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import context.ContextPart;
 import context.GameWindow;
@@ -17,36 +17,32 @@ import engine.common.event.GameEvent;
 
 /**
  * A container for all of the {@link ContextPart}-to-{@link ContextPart} queues.
- * 
+ *
  * @author Jay
  */
 public final class ContextQueues {
 
 	/**
-	 * The queue that {@link GameInput} would put {@link GameEvent}s into. These
-	 * {@link GameEvent}s should then be handled in {@link GameLogic}.
+	 * The queue that {@link GameInput} would put {@link GameEvent}s into. These {@link GameEvent}s should then be
+	 * handled in {@link GameLogic}.
 	 */
-	public final Queue<GameEvent> inputToLogic = new ArrayBlockingQueue<>(50);
-	public final Queue<GameEvent> logicToVisuals = new ArrayBlockingQueue<>(50);
-	public final Queue<GameEvent> logicToAudio = new ArrayBlockingQueue<>(50);
+	public final Queue<GameEvent> inputToLogic = new ConcurrentLinkedQueue<>();
+	public final Queue<GameEvent> logicToVisuals = new ConcurrentLinkedQueue<>();
+	public final Queue<GameEvent> logicToAudio = new ConcurrentLinkedQueue<>();
 	/**
-	 * The input buffer is initialized by the {@link GameEngine} upon
-	 * initialization. The {@link GameWindow} has <code>Callbacks</code> that write
-	 * to this input buffer. {@link GameInput} reads from the input buffer and
-	 * transforms them into other {@link GameEvent}s in the {@link #handleAll()}
-	 * function.
+	 * The input buffer is initialized by the {@link GameEngine} upon initialization. The {@link GameWindow} has
+	 * <code>Callbacks</code> that write to this input buffer. {@link GameInput} reads from the input buffer and
+	 * transforms them into other {@link GameEvent}s in the {@link GameInput#handleAll()} function.
 	 */
 	public final Queue<GameInputEvent> inputEventBuffer;
 	/**
-	 * The network buffer is initialized by the {@link GameEngine} upon
-	 * initialization if networking is enabled. The {@link UDPReceiver} writes to
-	 * this network buffer whenever a {@link DatagramPacket} arrives on the
-	 * {@link DatagramSocket} initialized in the {@link GameEngine}.
-	 * {@link GameInput} reads from the network buffer and transforms them into
-	 * other {@link GameEvent}s in the {@link #handleAll()} function.
+	 * The network buffer is initialized by the {@link GameEngine} upon initialization if networking is enabled. The
+	 * {@link UDPReceiver} writes to this network buffer whenever a {@link DatagramPacket} arrives on the {@link
+	 * DatagramSocket} initialized in the {@link GameEngine}. {@link GameInput} reads from the network buffer and
+	 * transforms them into other {@link GameEvent}s in the {@link GameInput#handleAll()} function.
 	 */
 	public final Queue<PacketReceivedInputEvent> networkReceiveBuffer;
-
+	
 	public ContextQueues(Queue<GameInputEvent> inputEventBuffer, Queue<PacketReceivedInputEvent> networkReceiveBuffer) {
 		this.inputEventBuffer = inputEventBuffer;
 		this.networkReceiveBuffer = networkReceiveBuffer;
