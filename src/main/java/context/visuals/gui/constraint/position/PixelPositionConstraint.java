9fdcf90c94b5d3173be8561720d8a5ab2f38d680
Lunkle <bobbob.donny@gmail.com>
2022-09-23 17:04:04 -0400

    Add supplier option to PixelPositionConstraint for dynamic position

--- src/main/java/context/visuals/gui/constraint/position/PixelPositionConstraint.java
+++ src/main/java/context/visuals/gui/constraint/position/PixelPositionConstraint.java
@@ -1,11 +1,13 @@
 package context.visuals.gui.constraint.position;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 import context.visuals.gui.constraint.dimension.GuiDimensionConstraint;
 
 public class PixelPositionConstraint extends GuiPositionConstraint {
 
+	private Supplier<Float> pixelsSupplier;
 	private float pixels;
 	private GuiDimensionConstraint dimensionConstraint;
 
@@ -13,13 +15,26 @@ public class PixelPositionConstraint extends GuiPositionConstraint {
 		this.pixels = pixels;
 	}
 
+	public PixelPositionConstraint(Supplier<Float> pixelsSupplier) {
+		this.pixelsSupplier = pixelsSupplier;
+	}
+
+	public PixelPositionConstraint(Supplier<Float> pixelsSupplier, GuiDimensionConstraint dimensionConstraint) {
+		this(pixelsSupplier);
+		this.dimensionConstraint = Objects.requireNonNull(dimensionConstraint);
+	}
+
+	//TODO @JaryJay: explain this
 	public PixelPositionConstraint(float pixels, GuiDimensionConstraint dimensionConstraint) {
-		this.pixels = pixels;
+		this(pixels);
 		this.dimensionConstraint = Objects.requireNonNull(dimensionConstraint);
 	}
 
 	@Override
 	public float get(float start, float end) {
+		if (pixelsSupplier != null) {
+			pixels = pixelsSupplier.get();
+		}
 		return dimensionConstraint == null ? start + pixels : end - pixels - dimensionConstraint.get(start, end);
 	}
 
